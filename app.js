5df21924cfe7a04b16491177f4d43a2156583228
tiidelab <info@techspecialistlimited.com>
2020-04-16 02:01:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const users = require('./users.json');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(users);
+});
+
+
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`) );
